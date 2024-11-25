795201984728988d01895cdb87fb52d8cabb78d4
Dipen <dipendraboyaju222@gmail.com>
2024-11-25 20:55:22 +0545

    change

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors({
   origin: 'https://crypto-tally.vercel.app',
   // origin: 'http://localhost:5173',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  methods: 'GET,POST',
   credentials: true,
 }));
 app.use(express.json())

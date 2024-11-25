2b71be6ae0591a1b050c38a0f01c129513527b8d
Dipen <dipendraboyaju222@gmail.com>
2024-11-25 20:52:48 +0545

    change

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(cors({
-  origin: 'https://crypto-tally-backend.vercel.app',
+  origin: 'https://crypto-tally.vercel.app',
   // origin: 'http://localhost:5173',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
